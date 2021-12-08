7826f3fb667471638d85e8fdc72ff04deb9542be
Bruno Cardoso <62511009+bruno-s-cardoso@users.noreply.github.com>
2021-12-08 12:34:15 -0300

    Initial files

---
+++ bind9-webmin/docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# start webmin
+exec /etc/webmin/start
+
+# allow arguments to be passed to named
+if [[ "${1:0:1}" == '-' ]]; then
+    EXTRA_ARGS="${*}"
+    set --
+elif [[ "${1}" == "named" || "${1}" == "$(command -v named)" ]]; then
+    EXTRA_ARGS="${*:2}"
+    set --
+fi
+
+# default behaviour is to launch named
+if [[ -z "${1}" ]]; then
+    echo "Starting named..."
+    echo "exec $(which named) -g \"${EXTRA_ARGS}\""
+    exec $(command -v named) -g ${EXTRA_ARGS}
+else
+    exec "${@}"
+fi
